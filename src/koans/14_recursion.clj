88b5820a96a248ff7d8b557a43e4aad206f675b9
Nik <nikhil.murthy.m@gmail.com>
2024-04-23 19:33:13 +1000

    Finishing up some more

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,18 +3,23 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= (count coll) 1)
+    coll
+    (loop [acc '()
+           remaining coll]
+      (conj acc (first coll))
+      (recur acc (rest coll)))))
 
 (defn factorial [n]
   __)
